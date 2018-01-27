727a2dcd5a89960bfb7ee8a899047f9ef2e5eed5
Hira <15030004@lums.edu.pk>
2018-01-27 17:24:52 -0500

    adding folders

---
+++ run_simulator.py
@@ -0,0 +1,16 @@
+import os
+import sys
+import pdb
+
+def main():
+    pass
+
+
+#users class
+#stats class
+#network state class
+#assume you have 10 users
+
+if __name__ == "__main__":
+    main()
+
