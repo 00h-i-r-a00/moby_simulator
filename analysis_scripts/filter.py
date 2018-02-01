344b73b65b5b1d41d84523b3efef1a538846ef06
Amogh Pradeep <amoghbl1@gmail.com>
2018-02-01 15:51:49 -0500

    Split file into n times 24 based on how many days(n) are given. Apply towers filter to it.

---
+++ analysis_scripts/filter.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+import sys
+
+def main():
+    days = sys.argv[1:]
+    print "Processing days: ", days
+    towers = []
+    with open("towers") as data:
+        for entry in data:
+            tid = entry.split()[0]
+            towers.append(tid)
+    print "Found towers: ", len(towers)
+    for i in xrange(0, len(days)):
+        print "Splitting ", days[i], " based on hours"
+        with open(days[i]) as data:
+            for entry in data:
+                hour, tid, users = entry.strip().split(',')
+                if tid in towers:
+                    with open(str(i)+"_"+hour, "a+") as outfile:
+                        outfile.write(entry)
+            print "Done with ", days[i]
+
+if __name__ == "__main__":
+    main()
