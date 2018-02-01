e6897fb488d70e19a755d7ccc8193c6f2a1865bc
Amogh Pradeep <amoghbl1@gmail.com>
2018-02-01 15:08:12 -0500

    Minor fix

--- analysis_scripts/get_towers_in_region.py
+++ analysis_scripts/get_towers_in_region.py
@@ -18,7 +18,7 @@ def main():
                 tower_id = line[0]
                 tower_lat = float(line[1])
                 tower_long = float(line[2])
-                dist = math.sqrt((x - tower_lat)**2 + (y - tower_long)**2)
+                dist = math.sqrt((x[i] - tower_lat)**2 + (y[i] - tower_long)**2)
                 if(dist < threshold):
                     outfile.write(entry)
 
