b38ad04a79154c06d740ddc8c95dabc0666a2602
Amogh Pradeep <amoghbl1@gmail.com>
2018-02-05 15:19:42 -0500

    More opti

--- amogh_simulator.py
+++ amogh_simulator.py
@@ -6,7 +6,7 @@ DATA_FILE_FORMAT = ".twr"
 MESSAGE_FILE_FORMAT = ".msg"
 
 start_day = 0
-end_day = 1
+end_day = 3
 network_state_old = defaultdict(set)
 network_state_new = defaultdict(set)
 message_queue = defaultdict(dict)
@@ -25,6 +25,7 @@ class Message:
 
 def main():
     first_state = True
+    total_messages = 0
     global dirty_nodes
     global message_delivery_count
     for current_day in xrange(start_day, end_day):
@@ -33,7 +34,9 @@ def main():
             current_data_file = DATA_FILE_PREFIX + str(current_day) + "_" + str(current_hour) + DATA_FILE_FORMAT
             current_message_file = DATA_FILE_PREFIX + str(current_day) + "_" + str(current_hour) + MESSAGE_FILE_FORMAT
             users_this_hour = []
-            print "Message Delivery count: ", message_delivery_count
+            print "Message Delivery count: ", message_delivery_count, " of: ", total_messages
+            if total_messages > 0:
+                print "Delivery rate: ", (float(message_delivery_count) / total_messages)*100, "%"
             print "Processing hour: ", current_hour
             with open(current_data_file) as data:
                 for entry in data:
@@ -64,38 +67,39 @@ def main():
                     msg = Message(id, int(ttl), src, dst, hop, trust)
                     message_queue[src][msg.id] = msg
                     dirty_nodes.append(src)
+                    total_messages += 1
             if not first_state:
+                changes_added = []
+                changes_removed = []
                 for tower in network_state_new.keys():
                     # Nodes that are new to a tower need to trigger msg exchanges, ignore this for first run
                     # as the first run marks all of them as new!
                     transitions_added = network_state_new[tower].difference(network_state_old[tower])
+                    transitions_removed = network_state_new[tower].difference(network_state_old[tower])
+                    changes_added += transitions_added
+                    changes_removed += transitions_removed
                     dirty_nodes += transitions_added
+            print "Network changes added: ", changes_added, " removed: ", changes_removed
             first_state = False
             print "Simulating message exchange on all nodes that belong to a network."
             for tower in network_state_new.keys():
                 users_in_tower = network_state_new[tower]
-                temp_dirty_bit = False
-                for user in users_in_tower:
-                    if user in dirty_nodes:
-                        temp_dirty_bit = True
-                        break
-                if temp_dirty_bit:
-                    # print "Perform exchange for tower, userslen. ", tower, len(users_in_tower)
-                    perform_message_exchanges(users_in_tower)
-                    clean_users(users_in_tower)
+                if perform_message_exchanges(users_in_tower):
+                    dirty_nodes += users_in_tower
             print "Messages for this hour sent, message exchanges complete. Perform destination check"
             for user, mq in message_queue.iteritems():
                 dellist = []
                 for key, msg in mq.iteritems():
                     # print "Dst: ", msg.dst, "User:", user
-                    if msg.dst == user and msg.id not in message_delivered[user]:
-                        message_delivery_count += 1
-                        message_delivered[user].append(msg.id)
-                        msg.ttl = 60
-                    elif msg.ttl > 1:
-                        msg.ttl -= 1
-                    else:
-                        dellist.append(msg.id)
+                    if msg.id not in message_delivered[user]:
+                        if msg.dst == user and msg.id:
+                            message_delivery_count += 1
+                            message_delivered[user].append(msg.id)
+                            msg.ttl = 60
+                        elif msg.ttl > 1:
+                            msg.ttl -= 1
+                        else:
+                            dellist.append(msg.id)
                 for id in dellist:
                     del message_queue[user][id]
             print "Updaing old state to new state."
@@ -110,17 +114,18 @@ def clean_users(users):
     dirty_nodes = new_dirty
 
 def perform_message_exchanges(users):
-    print "Exchange for: ", len(users)
-    for u1 in users:
+    dirty = list(set(users).intersection(dirty_nodes))
+    for u1 in dirty:
         for u2 in users:
+            if u1 == u2:
+                continue
             mq1 = message_queue[u1]
             mq2 = message_queue[u2]
             for key in mq1.keys():
-                if key not in mq2:
-                    mq2[key] = mq1[key]
+                mq2[key] = mq1[key]
             for key in mq2.keys():
-                if key not in mq1:
-                    mq1[key] = mq2[key]
+                mq1[key] = mq2[key]
+    return len(dirty) > 0
                     # Any of the trust score changing logic should come here.
     # print "Resulting message queue length: ", len(mq)
 
