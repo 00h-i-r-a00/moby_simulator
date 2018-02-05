b5b249b3bc95393aa02498072576a8185be4afeb
Amogh Pradeep <amoghbl1@gmail.com>
2018-02-05 12:13:02 -0500

    Message queue optimisation

--- amogh_simulator.py
+++ amogh_simulator.py
@@ -9,7 +9,7 @@ start_day = 0
 end_day = 1
 network_state_old = defaultdict(set)
 network_state_new = defaultdict(set)
-message_queue = defaultdict(list)
+message_queue = defaultdict(dict)
 message_delivered = defaultdict(list)
 message_delivery_count = 0
 dirty_nodes = []
@@ -62,7 +62,7 @@ def main():
                     # ID, TTL, Source, Destination, hop, trust
                     id, ttl, src, dst, hop, trust = entry.strip().split(",")
                     msg = Message(id, int(ttl), src, dst, hop, trust)
-                    message_queue[src].append(msg)
+                    message_queue[src][msg.id] = msg
                     dirty_nodes.append(src)
             if not first_state:
                 for tower in network_state_new.keys():
@@ -80,12 +80,13 @@ def main():
                         temp_dirty_bit = True
                         break
                 if temp_dirty_bit:
-                    print "Perform exchange for tower, userslen. ", tower, len(users_in_tower)
+                    # print "Perform exchange for tower, userslen. ", tower, len(users_in_tower)
                     perform_message_exchanges(users_in_tower)
                     clean_users(users_in_tower)
             print "Messages for this hour sent, message exchanges complete. Perform destination check"
             for user, mq in message_queue.iteritems():
-                for msg in mq:
+                dellist = []
+                for key, msg in mq.iteritems():
                     # print "Dst: ", msg.dst, "User:", user
                     if msg.dst == user and msg.id not in message_delivered[user]:
                         message_delivery_count += 1
@@ -94,7 +95,9 @@ def main():
                     elif msg.ttl > 1:
                         msg.ttl -= 1
                     else:
-                        message_queue[user].remove(msg)
+                        dellist.append(msg.id)
+                for id in dellist:
+                    del message_queue[user][id]
             print "Updaing old state to new state."
             network_state_old = network_state_new
 
@@ -112,15 +115,13 @@ def perform_message_exchanges(users):
         for u2 in users:
             mq1 = message_queue[u1]
             mq2 = message_queue[u2]
-            mq1ids = [i.id for i in mq1]
-            mq2ids = [i.id for i in mq2]
-            for i in xrange(0, len(mq2ids)):
-                if mq2ids[i] not in mq1ids:
-                    mq1.append(mq2[i])
+            for key in mq1.keys():
+                if key not in mq2:
+                    mq2[key] = mq1[key]
+            for key in mq2.keys():
+                if key not in mq1:
+                    mq1[key] = mq2[key]
                     # Any of the trust score changing logic should come here.
-            for i in xrange(0, len(mq1ids)):
-                if mq1ids[i] not in mq2ids:
-                    mq2.append(mq1[i])
     # print "Resulting message queue length: ", len(mq)
 
 if __name__ == "__main__":
