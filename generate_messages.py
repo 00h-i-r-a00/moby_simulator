cfc7ffd133254e905c896ddf6ce446f4441041d2
Amogh Pradeep <amoghbl1@gmail.com>
2018-02-07 14:04:35 -0500

    Number of messages flag added to generate_messages.

--- generate_messages.py
+++ generate_messages.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python
-import sys, random
+import sys, random, argparse
 
 DATA_FILE_PREFIX = "data/"
 DATA_FILE_FORMAT = ".twr"
 MESSAGE_FILE_FORMAT = ".msg"
 start_day = 0
 end_day = 3
-MESSAGES_TO_GENERATE = 100
 message_id_start = DATA_FILE_PREFIX
 
 def main():
+    parser = argparse.ArgumentParser(description='Moby message generation script script.')
+    parser.add_argument('--number', help='Number of messages to generate', type=int, nargs='?', default=1000)
+    args = parser.parse_args(sys.argv[1:])
+    number_of_messages = args.number
     for city in xrange(0, 4):
         for current_day in xrange(start_day, end_day):
             for current_hour in xrange(0,24):
@@ -25,10 +28,10 @@ def main():
                 print "Users in all towers(double counted):", len(users_this_hour)
                 users_this_hour = set(users_this_hour)
                 print "Unique users seen this hour: ", len(users_this_hour)
-                print "Generating: ", current_message_file, MESSAGES_TO_GENERATE
+                print "Generating: ", current_message_file, number_of_messages
                 # ID, TTL, Source, Destination, hop, trust
                 with open(current_message_file, "w") as out_file:
-                    for i in xrange(0, MESSAGES_TO_GENERATE):
+                    for i in xrange(0, number_of_messages):
                         id = DATA_FILE_PREFIX + str(current_hour) + "_" + str(i)
                         src, dst = random.sample(users_this_hour, 2)
                         src = str(src)
