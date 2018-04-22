78bd614f4a79d056c6217f12df6bca96e8d52377
Amogh Pradeep <amoghbl1@gmail.com>
2018-04-22 22:45:32 +0530

    Add run number and number of messages to generate_configs

--- generate_configs.py
+++ generate_configs.py
@@ -6,11 +6,13 @@ import os
 achtungs = ["achtung02", "achtung03", "achtung04", "achtung05", "achtung06", "achtung07", "achtung12", "achtung13", "achtung14", "achtung15", "achtung16", "achtung17"]
 achtungpool = itertools.cycle(achtungs)
 
+run_number = 0
 ttls = [12, 24, 36, 48, 60, 72]
 start_days = [100]
 number_of_days = [3]
 cities = [0, 1, 2, 3]
 cooldowns = [12, 24, 36]
+number_of_messages = [100000]
 
 def main():
     config = {}
@@ -24,16 +26,19 @@ def main():
             for val_nod in number_of_days:
                 for val_city in cities:
                     for val_cd in cooldowns:
-                        current_achtung = next(achtungpool)
-                        config["ttl"] = val_ttl
-                        config["start_day"] = val_start
-                        config["end_day"] = val_start+val_nod
-                        config["city"] = val_city
-                        config["cooldown"] = val_cd
-                        config_ctr += 1
-                        with open(current_achtung+".json", 'a+') as outfile:
-                            outfile.write(",")
-                            json.dump(config, outfile)
+                        for val_nm in number_of_messages:
+                            current_achtung = next(achtungpool)
+                            config["ttl"] = val_ttl
+                            config["start-day"] = val_start
+                            config["end-day"] = val_start+val_nod
+                            config["city-number"] = val_city
+                            config["cooldown"] = val_cd
+                            config["number"] = val_nm
+                            config["configuration"] = str(run_number) + "_" + str(config_ctr)
+                            config_ctr += 1
+                            with open(current_achtung+".json", 'a+') as outfile:
+                                outfile.write("!")
+                                json.dump(config, outfile)
 
     print config_ctr, "Uploading configs."
     os.system("scp achtung*.json achtung:")
