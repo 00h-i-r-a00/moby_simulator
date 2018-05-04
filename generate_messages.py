07d534fb16afb30d4feb41ea2ae6ec957bd52c52
00h-i-r-a00 <15030004@lums.edu.pk>
2018-05-04 11:26:11 -0400

    added distributions + other changes

--- generate_messages.py
+++ generate_messages.py
@@ -1,31 +1,36 @@
 #!/usr/bin/env python
+from __future__ import division
 import sys, random, argparse
 from collections import defaultdict
 import pdb
+import math
+import numpy as np
 
 DATA_FILE_PREFIX = "data/"
 SEED_FILE_PREFIX = "data/seeds/"
 DATA_FILE_FORMAT = ".twr"
 CONFIGURATION_FILE_FORMAT = ".config"
 message_id_start = DATA_FILE_PREFIX
-
+overall_network_state = defaultdict(dict)
 def main():
     userpool = defaultdict(int)
-    active_userpool_per_hour = defaultdict(int)
+    active_userpool_per_hour = {}
     parser = argparse.ArgumentParser(description='Moby message generation script script.')
     parser.add_argument('--number', help='Number of messages to generate', type=int, nargs='?', default=1000)
     parser.add_argument('--start-day', help='start day of the year', type=int, nargs='?', default=0)
     parser.add_argument('--end-day', help='end day of the year', type=int, nargs='?', default=None)
     parser.add_argument('--configuration', help='Configuration and message file id', type=str, nargs='?', default=0)
     parser.add_argument('--city-number', help='City to generate messages for', type=int, nargs='?', default=0)
-    parser.add_argument('--threshold', help='Minimum occourances to be considered a legit user', type=int, nargs='?', default=0)
+    parser.add_argument('--threshold', help='Minimum occourances to be considered a legit user', type=int, nargs='?', default=24)
     parser.add_argument('--cooldown', help='Cooldown hours, messages distributed over total hours - cooldown hours.', type=int, nargs='?', default=12)
     parser.add_argument('--ttl', help='The time to live to be used for the messages', type=int, nargs='?', default=72)
     parser.add_argument('--seed', help='Number to use for random seeding', type=int, nargs='?', default=3007052)
     parser.add_argument('--queuesize', help='0 if no queuesize else a specific number with the queuesize value', type=int, nargs='?', default=0)
-    parser.add_argument('--percentagehoursactive', help='Percentage of hours the destinations stay active', type=int, nargs='?', default=10)
+    parser.add_argument('--percentagehoursactive', help='Percentage of hours the destinations stay active', type=int, nargs='?', default=100)
     parser.add_argument('--deliveryratiotype', help='1 if total_messages == upto that hour; 2 if total_messages == total number of messages in all hours', type=int, nargs='?', default=1)
-    parser.add_argument('--messagegenerationtype', help='Original Criteria or Selectively changing sources and destinations',      type=int, nargs='?', default=1)
+    parser.add_argument('--messagegenerationtype', help='Original Criteria or Selectively changing sources and destinations', type=int, nargs='?', default=1)
+    parser.add_argument('--distributiontype', help='2 types -> "uniform" or "user-activity-based" ; used in conjunction with messagegenerationtype', type=str, nargs='?', default='uniform')
+
     args = parser.parse_args(sys.argv[1:])
     number_of_messages = args.number
     start_day = args.start_day
@@ -42,6 +47,7 @@ def main():
     percentage_hours_active = args.percentagehoursactive
     message_generation_type = args.messagegenerationtype
     deliveryratiotype = args.deliveryratiotype
+    distributiontype = args.distributiontype
 
     message_sending_hours = ((end_day - start_day) * 24) - cooldown
 
@@ -49,7 +55,7 @@ def main():
     h = 0
     print message_sending_hours
     print "Configuration (configuration, start, end, number, city, threshold, sending hours): ", configuration, start_day, end_day, number_of_messages, city, threshold, message_sending_hours
-    
+
     for current_day in xrange(start_day, end_day):
         for current_hour in xrange(0,24):
             current_data_file = DATA_FILE_PREFIX + str(city) + "/" + str(current_day) + "_" + str(current_hour) + DATA_FILE_FORMAT
@@ -59,11 +65,12 @@ def main():
                     hour, tower_id, user_ids = entry.split(",")
                     user_ids = user_ids.strip()
                     users_this_hour += user_ids.split("|")
+                    overall_network_state[h][tower_id] = len(set(user_ids.split("|")))
             users_this_hour = set(users_this_hour)
             active_userpool_per_hour[h] = users_this_hour
 
             h += 1
-            
+
             for u in users_this_hour:
                 userpool[u] += 1
     dellist = []
@@ -97,9 +104,11 @@ def main():
         out_file.write(str(percentage_hours_active) + "\n")
         out_file.write(str(message_generation_type) + "\n")
         out_file.write(str(deliveryratiotype) + "\n")
+        out_file.write(str(distributiontype) + "\n")
+        out_file.write(str(threshold) + "\n")
 
     print "Generating: ", current_message_file
-    distribution = get_message_distribution(message_sending_hours, number_of_messages)
+    distribution = get_message_distribution(message_sending_hours, number_of_messages, distributiontype)
     # ID, TTL, Source, Destination, hop, trust
     random.seed(seed)
 
@@ -110,14 +119,17 @@ def main():
 
         with open(current_message_file, "a+") as out_file:
 
-            for i in xrange(message_number):
+            for i in xrange(int(math.ceil(message_number))):
 
                 id = DATA_FILE_PREFIX + str(hour) + "_" + str(i)
 
                 if message_generation_type == 1:
                     src, dst = random.sample(userpool.keys(), 2)
+                    print "Msg No: %d, Current hour %d --> Sources Pool Size: %d ; Destinations Pool Size: %d for threshold %d" % (i, hour, len(userpool), len(userpool), threshold)
                 elif message_generation_type == 2:
                     src, dst = random.sample(sources_pool, 1)[0], random.sample(destinations_pool, 1)[0]
+
+                    print "Msg No:: %d, Current hour % d --> Sources Pool Size: %d ; Destinations Pool Size: %d" % (i, hour, len(sources_pool), len(destinations_pool))
 
                 src = str(src)
                 dst = str(dst)
@@ -127,15 +139,71 @@ def main():
                 out_file.write(getline(hour, id, ttl, src, dst, hop, trust))
                 out_file.write("\n")
 
-def get_message_distribution(sending_hours, total_messages):
+def get_message_distribution(sending_hours, total_messages, dist_type):
     dictionary = {}
     # Uniform distrbution for now.
-    overflow = total_messages % sending_hours
-    for i in xrange(0, sending_hours):
-        dictionary[i] = total_messages / sending_hours
-    for i in xrange(0, overflow):
-        dictionary[i] += 1
-    return dictionary
+    if dist_type == 'uniform':
+
+        overflow = total_messages % sending_hours
+
+        for i in xrange(0, sending_hours):
+            dictionary[i] = total_messages / sending_hours
+        for i in xrange(0, overflow):
+            dictionary[i] += 1
+        return dictionary
+
+    elif dist_type == 'user_activity_based':
+        medians = {}
+        h = 0
+        total = 0
+        total_msgs_sent = 0
+        for i in xrange(0, sending_hours):
+            users = []
+            for tower, num_users in overall_network_state[i].iteritems():
+                users.append(num_users)
+
+            medians[i] = np.median(users)
+            total += medians[i]
+
+        total_msgs_sent = 0
+
+        for i in xrange(0, sending_hours):
+
+             dictionary[i] = int(math.ceil((medians[i]/total) * total_messages))
+             total_msgs_sent += dictionary[i]
+
+        overflow = total_messages - total_msgs_sent
+
+        for i in xrange(0, overflow):
+            dictionary[i] += 1
+        return dictionary
+
+    elif dist_type == 'total_users_based':
+
+        total_users = 0
+        users_per_hour = {}
+        total_msgs_sent = 0
+
+        for i in xrange(0, sending_hours):
+            users_sum = 0
+            for tower, num_users in overall_network_state[i].iteritems():
+                users_sum += num_users
+                
+            total_users += users_sum
+
+            users_per_hour[i] = users_sum
+
+        for i in xrange(0, sending_hours):
+            dictionary[i] = int(math.ceil((users_per_hour[i]/total_users) * total_messages))
+            total_msgs_sent += dictionary[i]     
+        
+        overflow = total_messages - total_msgs_sent
+
+       
+        for i in xrange(0, overflow):
+            dictionary[i] += 1
+           
+        return dictionary 
 
 def get_destinations_active_for_X_percentage_of_hours(x, active_user_pool_per_hour, current_hour):
 
@@ -145,7 +213,7 @@ def get_destinations_active_for_X_percentage_of_hours(x, active_user_pool_per_ho
     ##checking the number of hours the destinations are active beyond the current hour
 
     for hour in xrange(current_hour, len(active_user_pool_per_hour.keys())):
-        
+
         for user in active_user_pool_per_hour[hour]:
             destinations_active[user] += 1
 
@@ -156,6 +224,8 @@ def get_destinations_active_for_X_percentage_of_hours(x, active_user_pool_per_ho
 
         if active_percentage >= x:
             destinations_pool.append(user)
+   # destinations_pool = destinations_active.keys()
+
 
     return destinations_pool
 
