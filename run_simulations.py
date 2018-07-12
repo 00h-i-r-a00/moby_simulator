5dd62decfeda0aee1c2596aba7c02985b1204a0f
00h-i-r-a00 <15030004@lums.edu.pk>
2018-07-12 16:16:02 -0400

    removed the checks for usethreshold

--- run_simulations.py
+++ run_simulations.py
@@ -3,20 +3,23 @@ import socket, json
 import os
 
 def main():
+
     hostname = socket.gethostname()
     print hostname
+
     with open (hostname+".json") as configs:
+
         confs = configs.readline().strip().split("!")
         confs = confs[1:]
+
     for conf in confs:
+
         cnf = json.loads(conf)
         msg_gen_string = "./generate_messages.py "
+
         for key, value in cnf.iteritems():
-            if key == 'usethreshold':
-                if value == True:
-                    msg_gen_string += "--" + key + " "
-            else:        
-                msg_gen_string += "--" + key + " " + str(value) + " "
+             msg_gen_string += "--" + key + " " + str(value) + " "
+        
         print msg_gen_string
         os.system(msg_gen_string)
         print cnf
