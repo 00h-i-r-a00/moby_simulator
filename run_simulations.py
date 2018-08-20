c7e571374389ec644e7a85e132f75346cd1c2ec0
00h-i-r-a00 <15030004@lums.edu.pk>
2018-08-20 13:32:28 -0400

    check free memory available on each node to decide number of processes

--- run_simulations.py
+++ run_simulations.py
@@ -4,17 +4,29 @@ import os
 import socket
 import subprocess
 import time
+import math
 
-TOTAL_CONCURRENT_PROCESSES = 10
+##########finding number of processes to allocate
+
+proc_meminfo_output = subprocess.check_output('cat /proc/meminfo', shell=True)
+free_mem = int(str(proc_meminfo_output.splitlines()[1]).split()[1])
+avg_mem_consumption_per_process = 20 #over-estimating to be on the safe side; can be changed
+TOTAL_CONCURRENT_PROCESSES = math.floor(free_mem/(avg_mem_consumption_per_process * 1000000))
+
+########################
+
 SLEEP_TIME = 1 * 60
 
 def main():
+	
     hostname = socket.gethostname()
     print (hostname)
+    
     with open (hostname+".json") as configs:
         confs = configs.readline().strip().split("!")
         confs = confs[1:]
     conf_pointer = 0
+    
     while conf_pointer < len(confs):
         numer_of_moby_processes_cmd = 'ps aux | grep "python3 moby_simulator.py" | wc -l'
         process = subprocess.Popen(numer_of_moby_processes_cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
