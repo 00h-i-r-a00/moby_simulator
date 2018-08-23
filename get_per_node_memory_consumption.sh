afdeb8eab60d7be0c5238162cde3c93ab3548bfa
Amogh Pradeep <amoghbl1@gmail.com>
2018-08-22 21:34:11 -0400

    Add achtung 10 and 11 to all shell scripts.

--- get_per_node_memory_consumption.sh
+++ get_per_node_memory_consumption.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-for server in achtung02 achtung03 achtung04 achtung05 achtung06 achtung07 achtung12 achtung13 achtung14 achtung15 achtung16 achtung17; do
+for server in achtung02 achtung03 achtung04 achtung05 achtung06 achtung07 achtung10 achtung11 achtung12 achtung13 achtung14 achtung15  achtung16 achtung17; do
     ssh $server << 'EOF'
     num_proc=0
     sum_virt=0
--- killAllSimulations.sh
+++ killAllSimulations.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-for server in achtung02 achtung03 achtung04 achtung05 achtung06 achtung07 achtung12 achtung13 achtung14 achtung15 achtung16 achtung17; do
+for server in achtung02 achtung03 achtung04 achtung05 achtung06 achtung07 achtung10 achtung11 achtung12 achtung13 achtung14 achtung15  achtung16 achtung17; do
     ssh $server << EOF
-    cd moby_simulator
-    nohup ./kill_moby_simulator.sh > data/memory_logs/$server.csv &
+    killall python
 EOF
 done
