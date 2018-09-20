3efa1ebf82605fc193b745a2ef85a54ad5e56a3b
Amogh Pradeep <amoghbl1@gmail.com>
2018-09-20 11:42:11 -0400

    Change run_simulations to support a killswitch and new memory calculation.

--- run_simulations.py
+++ run_simulations.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import getpass
 import json
 import os
 import socket
@@ -11,22 +12,29 @@ SLEEP_TIME = 1 * 60
 
 def get_total_concurrent_processes():
     proc_meminfo_output = subprocess.check_output('cat /proc/meminfo', shell=True)
-    total_mem = subprocess.check_output("vmstat -n -s | grep 'total memory' | awk '{print $1}'", shell=True)
+    total_mem = float(subprocess.check_output("vmstat -n -s | grep 'total memory' | awk '{print $1}'", shell=True))
     used_mem = subprocess.check_output("vmstat -n -s | grep 'used memory' | awk '{print $1}'", shell=True)
     free_mem = int(total_mem) - int(used_mem)
-    avg_mem_consumption_per_process = 8
-    total_concurrent_processes = int(math.floor(free_mem/(avg_mem_consumption_per_process * 1024 * 1024)))
-    return total_concurrent_processes
+    avg_mem_consumption_per_process = 20 * 1024 * 1024
+    total_concurrent_processes = int(math.floor(free_mem/avg_mem_consumption_per_process))
+    max_total_concurrent_processes = int(math.floor(total_mem/(avg_mem_consumption_per_process * 2)))
+    return min(total_concurrent_processes, max_total_concurrent_processes)
 
 def main():
     #get current hostname
+    me = getpass.getuser()
+    killswitch = "/home/" + me + '/killswitch'
     hostname = socket.gethostname()
     print (hostname)
     with open (hostname+".json") as conf_file:
         confs = json.load(conf_file)
     conf_pointer = 0
     while conf_pointer < len(confs):
-        number_of_moby_processes_cmd = 'pgrep -f moby_simulator.py'
+        if(os.path.isfile(killswitch)):
+            print("Killswitch encountered!!")
+            subprocess.check_output("killall python3 -u " + me, shell=True)
+            return
+        number_of_moby_processes_cmd = 'pgrep -u `whoami` -f moby_simulator.py'
         running_moby_processes = len(subprocess.check_output(number_of_moby_processes_cmd, shell=True).split())
         TOTAL_CONCURRENT_PROCESSES = get_total_concurrent_processes()
         processes_to_schedule = TOTAL_CONCURRENT_PROCESSES - running_moby_processes
