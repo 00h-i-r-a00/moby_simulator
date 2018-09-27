7a6eb380880e64870d82947c4ec4b862392cca16
Amogh Pradeep <amoghbl1@gmail.com>
2018-09-27 11:12:49 -0400

    Add scripts to generate report for a given simulation.

--- map_results.py
+++ map_results.py
@@ -1,26 +1,56 @@
 #!/usr/bin/env python3
 import argparse
-from prettytable import PrettyTable
+import matplotlib.pyplot as plt
 import os
 import sys
 
 RESULTS_PREFIX = 'data/results/'
+REPORTS_PREFIX = 'data/reports/'
 RESULT_EXT = '.csv'
 MESSAGE_DELAYS_EXT = '_message_delays.csv'
 QUEUE_OCCUPANCY_EXT = '_queue_occupancy.csv'
 
 def main():
-    table = PrettyTable()
     parser = argparse.ArgumentParser(description='Moby results visualizer.')
     parser.add_argument('--run-number', help='Run number to be plotted.', type=int, nargs='?', default=0)
+    parser.add_argument('--quiet', help='Wether to call show or not.', type=bool, nargs='?', default=False)
     args = parser.parse_args(sys.argv[1:])
     run_number = args.run_number
+    quiet = args.quiet
     files = os.listdir(RESULTS_PREFIX)
     filtered_files = []
     for f in files:
         if f.startswith(str(run_number)) and f.endswith(RESULT_EXT) and (not f.endswith(MESSAGE_DELAYS_EXT)) and (not f.endswith(QUEUE_OCCUPANCY_EXT)):
             filtered_files.append(f.strip(RESULT_EXT))
+    filtered_files.sort(key=lambda x: int(x.split('_')[-1].strip(RESULT_EXT)))
+    config_ids = [i.split('_')[-1].strip(RESULT_EXT) for i in filtered_files]
     print("Plotting results for:", filtered_files)
+    ratios = []
+    for f in filtered_files:
+        with open(RESULTS_PREFIX + f + RESULT_EXT, 'r') as infile:
+            last = infile.readlines()[-1]
+            last = last.split(',')
+            delivered = last[-2]
+            total = last[-1]
+            ratios.append(float(delivered) / float(total))
+    plt.figure(1)
+    plt.style.use('ggplot')
+    plt.title('Results seen for run number ' + str(run_number))
+    plt.xlabel('Configuration ID')
+    plt.ylabel('Delivery Ratio at the end of the simulation')
+    xaxis = [i for i in range(0, len(config_ids))]
+    plt.plot(xaxis, ratios, marker='o')
+    plt.xticks(rotation=45)
+    plt.xticks(xaxis, config_ids)
+    REPORT_DIR = REPORTS_PREFIX + str(run_number) + "/"
+    if not os.path.exists(REPORT_DIR):
+        os.makedirs(REPORT_DIR)
+    plt.savefig(REPORT_DIR + "message_deliveries.png")
+    if not quiet:
+        plt.show()
+    plt.gcf().clear()
+    for f in filtered_files:
+        os.system("./plot_message_delays.py --quiet True --run-number " + f)
 
 if __name__ == "__main__":
     main()
--- plot_message_delays.py
+++ plot_message_delays.py
@@ -2,19 +2,24 @@
 import argparse
 from collections import defaultdict
 import numpy as np
+import os
 import matplotlib.pyplot as plt
 import sys
 
 RESULTS_PREFIX = 'data/results/'
+REPORTS_PREFIX = 'data/reports/'
 MESSAGE_DELAYS_EXT = '_message_delays.csv'
 
 def main():
     parser = argparse.ArgumentParser(description='Script to plot moby message delays into a histogram.')
     parser.add_argument('--run-number', help='Run number to be plotted.', type=str, nargs='?', default="")
+    parser.add_argument('--quiet', help='Wether to call show or not.', type=bool, nargs='?', default=False)
     args = parser.parse_args(sys.argv[1:])
     run_number = args.run_number
+    quiet = args.quiet
     data_file = RESULTS_PREFIX + run_number + MESSAGE_DELAYS_EXT
-    print("Plotting histogram for:", run_number)
+    REPORT_DIR = REPORTS_PREFIX + run_number.split("_")[0] + "/"
+    print("Plotting bar chart for:", run_number)
     msgs_seen = []
     delays = defaultdict(int)
     with open(data_file, 'r') as infile:
@@ -35,8 +40,12 @@ def main():
     plt.bar(x_vals, y_vals)
     plt.xlabel("Delays")
     plt.ylabel("Number of messages with delay")
-    plt.title("Histogram for message delays for run" + run_number)
-    plt.show()
+    plt.title("Message delays for run" + run_number)
+    if not quiet:
+        plt.show()
+    if not os.path.exists(REPORT_DIR):
+        os.makedirs(REPORT_DIR)
+    plt.savefig(REPORT_DIR + run_number.split("_")[-1] + ".png")
 
 if __name__ == "__main__":
     main()
