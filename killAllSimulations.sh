e4f5509b5c6d9abe40495cfc9892db0cb2ce1524
Amogh Pradeep <amoghbl1@gmail.com>
2019-10-16 07:51:52 -0400

    Kill java and python3 as well.

--- killAllSimulations.sh
+++ killAllSimulations.sh
@@ -2,5 +2,7 @@
 for server in achtung02 achtung03 achtung04 achtung05 achtung06 achtung07 achtung10 achtung11 achtung12 achtung13 achtung14 achtung15  achtung16 achtung17; do
     ssh $server << EOF
     killall python
+    killall java
+    killall python3
 EOF
 done
