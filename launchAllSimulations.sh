fd7a999f4f3a03d812237840d166e7c1eb81804b
Amogh Pradeep <amoghbl1@gmail.com>
2021-08-09 18:46:28 -0400

    Force bash

--- launchAllSimulations.sh
+++ launchAllSimulations.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 for server in achtung02 achtung03 achtung04 achtung05 achtung06 achtung07 achtung08 achtung09 achtung11 achtung12 achtung13 achtung14 achtung15  achtung16 achtung17; do
-    ssh $server << EOF
+    ssh $server -t bash << EOF
     echo "######################################################"
     hostname
     pkill -f monitor_moby_simulator.sh
-    cd moby_simulator
-    nohup ./run_simulations.py > data/logs/$server.nohup &
+    cd moby/moby_simulator
+    nohup ./run_simulations.py > data/logs/$server.nohup &!
 EOF
 done
