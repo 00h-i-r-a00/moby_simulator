d2e318542f456eb2eb2c6a75099fc5e0a5b357cb
Amogh Pradeep <amoghbl1@gmail.com>
2021-08-11 14:56:48 -0400

    Asking for bash doesn't work, just redirect errors to the nohup log, fixes it

--- launchAllSimulations.sh
+++ launchAllSimulations.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 for server in achtung02 achtung03 achtung04 achtung05 achtung06 achtung07 achtung08 achtung09 achtung11 achtung12 achtung13 achtung14 achtung15  achtung16 achtung17; do
-    ssh $server -t bash << EOF
-    echo "######################################################"
+    ssh $server << EOF
+    echo "######################################################" $SHELL
     hostname
     pkill -f monitor_moby_simulator.sh
     cd moby/moby_simulator
-    nohup ./run_simulations.py > data/logs/$server.nohup &!
+    nohup ./run_simulations.py > data/logs/$server.nohup 2>&1 &!
 EOF
 done
