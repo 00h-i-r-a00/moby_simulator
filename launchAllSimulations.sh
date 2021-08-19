0b6d622dd2109efa3a39aaa39677a215c3919dea
Amogh Pradeep <amoghbl1@gmail.com>
2021-08-19 14:47:32 -0400

    Don't forget 10

--- launchAllSimulations.sh
+++ launchAllSimulations.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-for server in achtung02 achtung03 achtung04 achtung05 achtung06 achtung07 achtung08 achtung09 achtung11 achtung12 achtung13 achtung14 achtung15  achtung16 achtung17; do
+for server in achtung02 achtung03 achtung04 achtung05 achtung06 achtung07 achtung08 achtung09 achtung10 achtung11 achtung12 achtung13 achtung14 achtung15  achtung16 achtung17; do
     ssh $server << EOF
     echo "######################################################" $SHELL
     hostname
